4b9cc902ec663252a12e5a8fa038f4bc8f1fd20d
aiki <aiki.kage@gmail.com>
2021-08-03 11:59:15 +0900

    push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :email, :encrypted_password])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
-
-
-  
 end
