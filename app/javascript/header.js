4a1ec87f1b53f183c806a15b918de13e1cf2c307
aiki <aiki.kage@gmail.com>
2021-08-07 07:45:03 +0900

    header_js

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,17 +1,30 @@
-
 window.addEventListener('load',() => {
-  i = i + 1
-  const aaa = document.getElementsByClassName("text")[0];
-  aaa.addEventListener('mousemove', () => {
-    aaa.style.color = "red"
-    aaa.shadowBlur = 0;
+  const btn1 = document.getElementById("btn1");
+  const btn2 = document.getElementById("btn2");
+  const btn3 = document.getElementById("btn3");
+  const btn4 = document.getElementById("btn4");
+  btn1.addEventListener('mousemove',() => {
+    btn1.style.color = "red"
   });
-  aaa.addEventListener('mouseout', () => {
-    aaa.style.color = "black"
+  btn1.addEventListener('mouseout',() => {
+    btn1.style.color = "black"
+  });
+  btn2.addEventListener('mousemove',() => {
+    btn2.style.color = "red"
+  });
+  btn2.addEventListener('mouseout',() => {
+    btn2.style.color = "black"
+  });
+  btn3.addEventListener('mousemove',() => {
+    btn3.style.color = "red"
+  });
+  btn3.addEventListener('mouseout',() => {
+    btn3.style.color = "black"
+  });
+  btn4.addEventListener('mousemove',() => {
+    btn4.style.color = "red"
+  });
+  btn4.addEventListener('mouseout',() => {
+    btn4.style.color = "black"
   });
 });
-
-
-    // console.log("クリックされました");
-
-
